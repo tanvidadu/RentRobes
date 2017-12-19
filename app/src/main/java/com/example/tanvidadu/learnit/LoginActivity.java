c5b508e7c2de12913eee4a7afd499fb668bddbd8
tanvidadu <tanvidadu1998@gmail.com>
2017-12-19 06:03:24 +0530

    Login Activity error fixed

--- app/src/main/java/com/example/tanvidadu/learnit/LoginActivity.java
+++ app/src/main/java/com/example/tanvidadu/learnit/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.tanvidadu.learnit;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ import java.util.List;
 public class LoginActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
+    public static final String CustomerEmail = "CURRENT_CUSTOMER";
     private CustomerInfo customerInfo = new CustomerInfo();
 
     private FirebaseAuth firebaseAuth;
@@ -38,6 +40,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SharedPreferences settings = getSharedPreferences(CustomerEmail, 0);
+        CustomerPresent.setCustomerEmail( settings.getString("silentMode", null));
+        //setSilent(silent);
 
         LinearLayout hiddenLayout = (LinearLayout) findViewById(R.id.hiddenLayout);
         if(hiddenLayout != null){
@@ -95,6 +101,20 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         };
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(CustomerEmail, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("silentMode", CustomerPresent.getCustomerEmail());
+
+        // Commit the edits!
+        editor.commit();
     }
 
     @Override
@@ -230,8 +250,13 @@ public class LoginActivity extends AppCompatActivity {
     //Remove Welcome Page
     public void RemoveWelcomePage(){
         View myView = findViewById(R.id.hiddenLayout2);
-        ViewGroup parent = (ViewGroup) myView.getParent();
-        parent.removeView(myView);
+
+        try {
+            ViewGroup parent = (ViewGroup) myView.getParent();
+            parent.removeView(myView);
+        } catch (NullPointerException e){
+            
+        }
     }
 
     //Further to main Activity
@@ -243,4 +268,6 @@ public class LoginActivity extends AppCompatActivity {
         }
         startActivity(i);
     }
+
+
 }
